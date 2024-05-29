4f9c583ab51e865897fb763eacfe13f20fdd9044
tony1234567891011 <130993859+tony1234567891011@users.noreply.github.com>
2024-05-29 15:16:25 -0400

    Update DamageNumbers.java

--- DamageNumbers.java
+++ DamageNumbers.java
@@ -8,10 +8,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class DamageNumbers extends UI
 {
+    
     /**
      * Act - do whatever the DamageNumbers wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public DamageNumbers()
+    {
+        
+    }
     public void act()
     {
         // Add your action code here.
