e718f3a43f5acd6b065ff6771ab58964bc1906a6
tony1234567891011 <130993859+tony1234567891011@users.noreply.github.com>
2024-05-30 12:10:27 -0400

    animation
    a

--- Enemy.java
+++ Enemy.java
@@ -1,15 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class Enemy extends Entity {
-    private int hp;
-
+public abstract class Enemy extends Entity {
+    private int hp, mvtSpd = 2;
+    private String defaultFacing = "right";
     public Enemy() {
         super(Team.ENEMY, 100);
         this.hp = 50;  
     }
 
     public void act() {
-        
     }
 
     public void takeDamage(int damage) {
@@ -21,6 +20,47 @@ public class Enemy extends Entity {
 
     private void die() {
         getWorld().removeObject(this);
+    }
+    public void chasePlayer()
+    {
+        if(Player.returnX() < this.getX() && Player.returnY() > this.getY())
+        {
+            defaultFacing = "left";
+            setLocation(this.getX()-mvtSpd, this.getY()+mvtSpd);
+        }
+        else if(Player.returnX() < this.getX() && Player.returnY() < this.getY())
+        {
+            defaultFacing = "left";
+            setLocation(this.getX()-mvtSpd, this.getY()-mvtSpd);
+        }
+        else if(Player.returnX() > this.getX() && Player.returnY() > this.getY())
+        {
+            defaultFacing = "right";
+            setLocation(this.getX()+mvtSpd, this.getY()+mvtSpd);
+        }
+        else if(Player.returnX() > this.getX() && Player.returnY() < this.getY())
+        {
+            defaultFacing = "right";
+            setLocation(this.getX()+mvtSpd, this.getY()-mvtSpd);
+        }
+        else if(Player.returnX() < this.getX())
+        {
+            defaultFacing = "left";
+            setLocation(this.getX()-mvtSpd, this.getY());
+        }
+        else if(Player.returnX() > this.getX())
+        {
+            defaultFacing = "right";
+            setLocation(this.getX()+mvtSpd, this.getY());
+        }
+        else if(Player.returnY() > this.getY())
+        {
+            setLocation(this.getX(), this.getY()+mvtSpd);
+        }
+        else if(Player.returnY() < this.getY())
+        {
+            setLocation(this.getX(), this.getY()-mvtSpd);
+        }
     }
 }
 
--- Player.java
+++ Player.java
@@ -10,32 +10,51 @@ import java.util.List;
 public class Player extends Entity
 {
     
-    private int speed;
+    private int speed, frame = 0, acts = 0;
+    private static int x, y; // location of the Player.
+    private Animation right,left,down,up;
 
     public Player() {
         super(Team.ALLY, 100);
         speed = 5;
         hp = 100;
+        up = Animation.createAnimation(new GreenfootImage("Player.png"), 8, 1, 9, 64, 64);
+        left = Animation.createAnimation(new GreenfootImage("Player.png"), 9, 1, 9, 64, 64);
+        down = Animation.createAnimation(new GreenfootImage("Player.png"), 10, 1, 9, 64, 64);
+        right = Animation.createAnimation(new GreenfootImage("Player.png"), 11, 1, 9, 64, 64, 3);
+        setImage(up.getFrame(0));
     }
 
     public void act() {
         movePlayer();
         // Add other behaviours here (like checking for collisions, etc.)
+        attack(10);
     }
 
     private void movePlayer() {
+        if(acts%2 == 0)
+        {
+           frame = (frame+1)%(up.getAnimationLength()); 
+        }
+        acts++;
         if (Greenfoot.isKeyDown("w")) {
             setLocation(getX(), getY() - speed);
+            setImage(up.getFrame(frame));
         }
         if (Greenfoot.isKeyDown("s")) {
             setLocation(getX(), getY() + speed);
+            setImage(down.getFrame(frame));
         }
         if (Greenfoot.isKeyDown("a")) {
             setLocation(getX() - speed, getY());
+            setImage(left.getFrame(frame));
         }
         if (Greenfoot.isKeyDown("d")) {
             setLocation(getX() + speed, getY());
+            setImage(right.getFrame(frame));
         }
+        x = getX();
+        y = getY();
     }
 
     private void attack(int damage){
@@ -65,6 +84,14 @@ public class Player extends Entity
 
     public int getHp() {
         return hp;
+    }
+    public static int returnX()
+    {
+        return x;
+    }
+    public static int returnY()
+    {
+        return y;
     }
 }
 
