38fd026fe6405b064f297aeb356530922bea32e4
tony1234567891011 <130993859+tony1234567891011@users.noreply.github.com>
2024-06-01 19:34:28 -0400

    Added Aura Images:, animation and etc.
    Tinkered Mr Cohen's animation class to make Aura appear in more transparent way.

--- Aura.java
+++ Aura.java
@@ -3,16 +3,33 @@ import greenfoot.*;
 public class Aura extends SuperSmoothMover {
     private Actor owner;
     private boolean visible;
-
+    private Animation animate;
+    private int frame = 0, offset = 80, acts = 0;
     public Aura(Actor owner) {
         this.owner = owner;
         this.visible = false;
+        animate = Animation.createAnimation(new GreenfootImage("Aura.png"), 0, 1, 4, 205, 220, 0, 150);
+        setImage(animate.getFrame(0));
         updateVisibility();
     }
 
     public void act() {
-        setLocation(owner.getX(), owner.getY());
+        setLocation(owner.getX()-(offset/5), owner.getY()-offset);
+        animate();
         updateVisibility();
+        acts++;
+    }
+    
+    /**
+     * Animate.
+     */
+    public void animate()
+    {
+        if(acts%3 == 0)
+        {
+            frame = (frame+1) % (animate.getAnimationLength());
+        }
+        setImage(animate.getFrame(frame));
     }
 
     public void makeVisible() {
