ff9afba78a1c70afb990192b5e1f768f4424385e
OsmondLin1 <349757484@gapps.yrdsb.ca>
2024-06-04 11:09:12 -0400

    FIxed tile paint order

--- GameWorld.java
+++ GameWorld.java
@@ -18,7 +18,7 @@ public class GameWorld extends World
     public GameWorld()
     { 
         super(1200, 768, 1, false); 
-        setPaintOrder(Cursor.class, CollisionBox.class, TextBox.class, Picture.class, Cell.class, SuperStatBar.class, Projectile.class, Entity.class, Aura.class, PowerUp.class, Heal.class, CellEffect.class, Tile.class);
+        setPaintOrder(Cursor.class, CollisionBox.class, TextBox.class, Picture.class, Cell.class, SuperStatBar.class, Projectile.class, Entity.class, Aura.class, Drop.class, CellEffect.class, Tile.class);
         Mouse.setWorld(this);
         Tile.initializeTileDatabase();
         Room.initializeRoomDatabase();
